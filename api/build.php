1ddb2714962464a8ac378c0fd5de5de80954af32
Vinícius Carvalho <vcc180@gmail.com>
2026-04-23 14:20:39 -0300

    Atualização final

--- api/build.php
+++ api/build.php
@@ -1,12 +1,17 @@
 <?php
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
 
 include_once("../config.php");
 include_once("../libs/Database.class.php");
 
+header('Content-Type: application/json');
+
+header('Cache-Control: no-cache, no-store, must-revalidate');
+
 $db = new Database();
-$PROJETO = array(
+
+$projeto_id = intval($_REQUEST['projeto_id']); // segurança básica
+
+$PROJETO = [
     'project_title' => "",
     'project_name' => "",
     'project_path' => "",
@@ -17,89 +22,108 @@ $PROJETO = array(
     'db_password' => "",
     'MENUS' => [],
     'modulos' => []
-);
-$temp = $db->select('tbprojeto', "*", "WHERE projeto_id=" . $_REQUEST['projeto_id']);
+];
 
-if (count($temp) < 0) {
-    die(array('error' => 1, "msg" => "Não foi selecionado o projeto"));
+/*************************/
+// PROJETO
+/*************************/
+$temp = $db->select('tbprojeto', "*", "WHERE projeto_id=" . $projeto_id);
+
+if (count($temp) <= 0) {
+    echo json_encode(['error' => 1, "msg" => "Projeto não encontrado"]);
+    exit;
 }
 
-foreach ($temp as $key => $value) {
-    // print_r($value);
-    $PROJETO['project_title']  = $value['projeto_title'];
-    $PROJETO['project_name']  = $value['projeto_name'];
-    $PROJETO['project_path']  = $value['projeto_path'];
-    $PROJETO['project_url']  = $value['projeto_url'];
-    $PROJETO['db_hostname']  = $value['projeto_db_hostname'];
-    $PROJETO['db_database']  = $value['projeto_db_database'];
-    $PROJETO['db_user']  = $value['projeto_user'];
-    $PROJETO['db_password']  = $value['projeto_password'];
+$value = $temp[0];
+
+$PROJETO['project_title']  = $value['projeto_title'];
+$PROJETO['project_name']  = $value['projeto_name'];
+$PROJETO['project_path']  = $value['projeto_path'];
+$PROJETO['project_url']  = $value['projeto_url'];
+$PROJETO['db_hostname']  = $value['projeto_db_hostname'];
+$PROJETO['db_database']  = $value['projeto_db_database'];
+$PROJETO['db_user']  = $value['projeto_user'];
+$PROJETO['db_password']  = $value['projeto_password'];
+
+/*************************/
+// MENUS
+/*************************/
+$temp = $db->select('tbmenu', '*', "where menu_projeto_id=" . $projeto_id);
+$MENUS = [];
+
+foreach ($temp as $value) {
+    $MENUS[$value['menu_nome']] = [
+        "TITLE" => $value['menu_title'],
+        "SUB_MENU" => $value['menu_submenu'],
+        "ICON" => $value['menu_icon'],
+        "MENUS" => []
+    ];
 }
 
-//MENUS
-unset($temp);
-$temp = $db->select('tbmenu', '*', "where menu_projeto_id=" . $_REQUEST['projeto_id']);
-$MENUS = array();
+/*************************/
+// SUBMENU
+/*************************/
+$temp = $db->select('view_submenu', '*', "where menu_projeto_id=" . $projeto_id);
 
-if (count($temp) > 0) {
-    foreach ($temp as $key => $value) {
-        $MENUS[][$value['menu_nome']] = array(
-            "TITLE" => $value['menu_title'],
-            "SUB_MENU" => $value['menu_submenu'],
-            "ICON" => $value['menu_icon'],
-            "MENUS" => []
-        );
-    }
+foreach ($temp as $value) {
+    if (!isset($MENUS[$value['menu_nome']])) continue;
+
+    $MENUS[$value['menu_nome']]['MENUS'][$value['submenu_nome']] = [
+        "TITLE" => $value['submenu_title'],
+        "LINK" => $value['submenu_link'],
+    ];
 }
 
-//SUBMENU
-unset($temp);
-$temp = $db->select('view_submenu', '*', "where menu_projeto_id=" . $_REQUEST['projeto_id']);
-function searchMenu($search, $array)
-{
-    foreach ($array as $key => $value) {
-        if ($search === key($value)) {
-            return $key;
-        }
-    }
-    return -1;
+$PROJETO['MENUS'] = array_values($MENUS);
+
+/*************************/
+// MODULOS
+/*************************/
+$temp = $db->select('view_modulos', '*', "where modulos_projeto_id=" . $projeto_id);
+$MODULO = [];
+
+foreach ($temp as $value) {
+    $MODULO[$value['modulos_modulo']] = [
+        "modulo_title" => $value['modulos_title'],
+        "modulo" => $value['modulos_modulo'],
+        "controller" => $value['modulos_controller'],
+        "model" => $value['modulos_model'],
+        "table" => $value['modulos_table'],
+        "db_prefixo" => $value['modulos_db_prefixo'],
+        "primary_key" => $value['modulos_primary_key'],
+        "field_default" => $value['modulos_field_default'],
+        "field_status" => $value['modulos_field_status'],
+        "campos" => []
+    ];
 }
 
-if (count($temp) > 0) {
-    foreach ($temp as $key => $value) {
-        $chave = searchMenu($value['menu_nome'], $MENUS);
-        if ($chave < 0) {
-            continue;
-        }
-        $MENUS[$chave][$value['menu_nome']]['MENUS'][$value['submenu_nome']] = array(
-            "TITLE" => $value['submenu_title'],
-            "LINK" => $value['submenu_link'],
-        );
-    }
+/*************************/
+// CAMPOS MODULO
+/*************************/
+$temp = $db->select('view_modulo_campo', '*', "where projeto_id=" . $projeto_id);
+
+foreach ($temp as $value) {
+    $moduloNome = $value['modulos_modulo'];
+
+    if (!isset($MODULO[$moduloNome])) continue;
+
+    $MODULO[$moduloNome]['campos'][] = [
+        "title" => $value['modulo_campo_title'],
+        "nome" => $value['modulo_campo_nome'],
+        "tipo" => $value['tipo_campo_nome'],
+        "fk" => ($value['modulo_campo_fk'] == true) ? "true": "false",
+        "reference_table" => $value['modulo_campo_reference_table'],
+        "reference_key" => $value['modulo_campo_reference_key'],
+        "reference_option" => $value['modulo_campo_reference_option'],
+        "required" => ($value['modulo_campo_required'] == true) ? "true": "false",
+        "is_search" => ($value['modulo_campo_is_search']) ? "true": "false"
+    ];
 }
 
-$PROJETO['MENUS'] = $MENUS;
+$PROJETO['modulos'] = array_values($MODULO);
 
-//MODULOS
-$temp = $db->select('tbmodulos', '*', "where modulos_projeto_id=" . $_REQUEST['projeto_id']);
-$MODULO = array();
-
-if (count($temp) > 0) {
-    foreach ($temp as $key => $value) {
-        $MODULO[] = array(
-            "modulo_title" => $value['modulos_title'],
-            "modulo" => $value['modulos_modulo'],
-            "controller" => $value['modulos_controller'],
-            "model" => $value['modulos_model'],
-            "table" => $value['modulos_table'],
-            "db_prefixo" => $value['modulos_db_prefixo'],
-            "primary_key" => $value['modulos_primary_key'],
-            "field_default" => $value['modulos_field_default'],
-            "field_status" => $value['modulos_field_status'],
-            "campos" => array(),
-        );
-    }
-}
-
-$PROJETO['modulos'] = $MODULO;
-die(json_encode($PROJETO));
+/*************************/
+// OUTPUT
+/*************************/
+echo json_encode($PROJETO, JSON_PRETTY_PRINT);
+header('Content-Disposition: attachment; filename="' . $PROJETO['project_name'] .'.json"');
